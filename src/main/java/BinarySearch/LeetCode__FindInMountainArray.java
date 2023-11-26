f23c036f9223bde04f989a07a36782c6a0436014
Prashant Ghule <84400771+GhulePrashant@users.noreply.github.com>
2023-11-27 00:59:24 +0530

    day 3 : Binary Search - LeetCode problems

---
+++ src/main/java/BinarySearch/LeetCode__FindInMountainArray.java
@@ -0,0 +1,72 @@
+package BinarySearch;
+
+public class LeetCode__FindInMountainArray {
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,3,1};
+        int target = 3;
+
+        System.out.println(search(nums, target));
+    }
+
+    static int search(int[] arr, int target){
+        int peak = peakIndexInMountainArray(arr);
+        int firstTry = orderAgnosticBinarySearch(arr,target, 0, peak);
+        if (firstTry != -1)
+            return firstTry;
+
+        return orderAgnosticBinarySearch(arr, target, peak+1, arr.length-1);
+    }
+
+    private static int orderAgnosticBinarySearch(int[] arr, int target, int start, int end) {
+
+        // find whether the array is ascending or not
+        boolean isAscending = arr[start] < arr[end];    // if we use start element, there might be possibility that start & start+1 are same.
+
+        while(start<=end){      // loop until start becomes > end
+            int mid = start + (end - start)/2 ;  // finding mid element (start+end)/2 might exceed in a case where size is large
+
+            if (arr[mid] == target)
+                return mid;
+
+            if (isAscending) {
+                if (target < arr[mid]) {
+                    end = mid - 1;               // target is on left side of mid element
+                } else {
+                    start = mid + 1;            // target is on right side of mid element
+                }
+            }else{
+                if (target > arr[mid]) {
+                    end = mid - 1;               // target is on left side of mid element
+                } else {
+                    start = mid + 1;            // target is on right side of mid element
+                }
+            }
+        }
+        // element not found
+        return -1;
+    }
+
+    public static int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start < end){
+
+            int mid = start + (end - start)/2;
+
+            if (arr[mid] > arr[mid + 1]){
+                // descending part of array
+                end = mid ;
+            }else{
+                // ascending part of array
+                // need to look in right side hence
+                start = mid + 1;
+            }
+        }
+
+        // start and end are finding the largest element in the above two checks
+        // start == end will be our ans, hence we can return start or end
+        return start;
+    }
+}
---
+++ src/main/java/BinarySearch/LeetCode__PeakIndexInMountainArray.java
@@ -0,0 +1,87 @@
+package BinarySearch;
+
+/**
+ * https://leetcode.com/problems/find-peak-element/
+ * https://leetcode.com/problems/peak-index-in-a-mountain-array/
+ * Same solution can be used for both problems
+ *
+ * 852. Peak Index in a Mountain Array
+ *
+ * Medium
+ * Topics
+ * Companies
+ * An array arr is a mountain if the following properties hold:
+ *
+ * arr.length >= 3
+ * There exists some i with 0 < i < arr.length - 1 such that:
+ * arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+ * arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+ * Given a mountain array arr, return the index i such that arr[0] < arr[1] < ... < arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
+ *
+ * You must solve it in O(log(arr.length)) time complexity.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: arr = [0,1,0]
+ * Output: 1
+ * Example 2:
+ *
+ * Input: arr = [0,2,1,0]
+ * Output: 1
+ * Example 3:
+ *
+ * Input: arr = [0,10,5,2]
+ * Output: 1
+ *
+ *
+ * Constraints:
+ *
+ * 3 <= arr.length <= 105
+ * 0 <= arr[i] <= 106
+ * arr is guaranteed to be a mountain array.
+ */
+
+public class LeetCode__PeakIndexInMountainArray {
+    /**
+     * Approach
+     *
+     * we'll use binarySearch for this
+     * [0,10,5,2] 10 is ans, left side of 10 is ascending and right side is descending
+     * if arr[mid] > arr[mid +1] --> you're in descending side
+     * then end = mid for checking left side
+     * if arr[mid] < arr[mid + 1] --> you're in ascending part
+     * then start = mid + 1 for checking in right side
+     * when will loop break ?
+     * start==end will be the ans, so loop will run till start<end
+     */
+    public static void main(String[] args) {
+        int[] nums = {5,7,8,10,6,4,3};
+
+        System.out.println(peakIndexInMountainArray(nums));
+    }
+
+    public static int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start < end){
+
+            int mid = start + (end - start)/2;
+
+            if (arr[mid] > arr[mid + 1]){
+                // descending part of array
+                end = mid ;
+            }else{
+                // ascending part of array
+                // need to look in right side hence
+                start = mid + 1;
+            }
+        }
+
+        // start and end are finding the largest element in the above two checks
+        // start == end will be our ans, hence we can return start or end
+        return start;
+    }
+}
